6e0c2f1f4a2684e251513a705eaef377ab01b52f
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2026-03-20 15:48:24 -0700

    baymesh: update to 2.7.21.697b14d with MeshControlModule

--- types/resources.ts
+++ types/resources.ts
@@ -2,7 +2,7 @@ import type { FirmwareResource } from './api';
 
 // Remove the OfflineHardwareList since it's now in /public/data/hardware-list.json
 
-const currentPrereleaseId = '2.7.21.2e4dc86';
+const currentPrereleaseId = '2.7.21.697b14d';
 
 export const showPrerelease = true;
 
