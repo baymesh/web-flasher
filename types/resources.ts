0324a817ec46cfef81dfe0f330972ccaf16a37d2
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2026-03-19 23:23:53 -0700

    baymesh: update to 2.7.21.12df092 with correct directory structure

--- types/resources.ts
+++ types/resources.ts
@@ -2,7 +2,7 @@ import type { FirmwareResource } from './api';
 
 // Remove the OfflineHardwareList since it's now in /public/data/hardware-list.json
 
-const currentPrereleaseId = '2.7.21.77bf998';
+const currentPrereleaseId = '2.7.21.12df092';
 
 export const showPrerelease = true;
 
